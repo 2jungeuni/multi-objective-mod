7d6fe4156068fc87ffb7fcf37997edb4fb9cedc2
2jungeuni <jungeunlee14@gmail.com>
2024-07-18 12:49:04 +0900

    .

--- README.md
+++ README.md
@@ -1,16 +1,59 @@
 # multi-objective-mod
+Moiblity-on-demand system for multiple vehicles.
+
+### Objective
+Board as many passengers as possible, satisfying constraints.
+
+### Constraints
+1. There are N vehicles.
+2. A location must be visited only once.
+3. There is no depot (It can start from anywhere and arrive anywhere).
+4. There is a working time for each deliver.
+5. There is a capacity of vehicles.
+6. A penalty is given for the number of locations that cannot be visited.
+
+### Requirements
+```bash
+sudo apt update
+sudo apt install pybind11-dev
+pip install gurobipy==10.0.3
+```
+
+### My own python modules
+It is for using my route planning algorithm (``planner.cpp``) in Python3. pybind11 is a header library faciliating seamless C++ and Python interoperability with minimal code, making it straightforward to expose C++ functions and classes to Python.
 
 ### Compile
 ```bash
 c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) astar.h astar.cpp planner.cpp -o planner.so
 ```
 
-### :runner: Run demo code
+### Run demo code
 If you want to get the optimal cost,
 ```python
-python3 main.py -v ${number_of_vehicles} -w ${alpha} ${beta} ${gamma} -pudo ${pickup_location_1} ${dropoff_location_1} -pudo ${pickup_location_2} ${dropoff_location_2}
+python3 main.py -v ${number_of_vehicles} -w ${alpha} ${beta} ${gamma} -pudo ${pickup_location_1} ${dropoff_location_1} -pudo ${pickup_location_2} ${dropoff_location_2} -c ${capacity} -p ${penalty} -t ${time_limit}
 ```
 For example,
 ```python3
-python3 main.py -v 3 -w 1 0 0 -pudo 47 52 -pudo 244 642 -pudo 508 1572 -pudo 60 342 -pudo 440 1014
+python3 main.py -v 3 -w 0 1 0 -pudo 47 52 -pudo 244 642 -pudo 508 1572 -pudo 60 342 -pudo 440 1014 -c 10 -p 0 -t 3600
 ```
+- Delivery calls
+
+| user ID | pickup location | dropoff location |
+|:-------:|:---------------:|:----------------:|
+|    0    |       47        |        52        |
+|    1    |       244       |       642        |
+|    2    |       508       |       1572       |
+|    3    |       60        |       342        |
+|    4    |       440       |       1014       |
+- Result
+
+| car |              route              | users | capacity | travel time (seconds) |
+|:---:|:-------------------------------:|:-----:|:--------:|:---------------------:|
+|  0  |          508&rarr;1572          |   2   |    1     |        2113.92        |
+|  1  | 244&rarr;440&rarr;642&rarr;1014 | 1, 4  |    2     |        1874.46        |
+|  2  |   60&rarr;47&rarr;52&rarr;342   | 3, 0  |    2     |        663.35         |
+
+<p align="center">
+<img src="./img/seq_time_limit.png" width="120" height="250"> <img src="./img/paths_time_limit.png" width="120" height="250">
+</p>
+The left is sequences of locations and the right is detailed paths.
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+# built-in
+import os
+import sys
+import copy
 import pickle
+import logging
 import argparse
 import numpy as np
 import gurobipy as gp
-from planner import *
 from gurobipy import *
-from sklearn.neighbors import BallTree
+from tabulate import tabulate
+
+# my own
+from planner import *
+from visualization import visualization
+
+# initialize the logger
+logger = logging.getLogger("Multi objective mod system")
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 # status dictionary
 status_dict = {1: "loaded",
@@ -25,18 +44,18 @@ status_dict = {1: "loaded",
                16: "work limit",
                17: "memory limit"}
 
-
-def create_data_model(func, num_veh, pudo, alpha=1, beta=0, gamma=0):
+def create_data_model(func, num_veh, pudo, penalty, alpha=1, beta=0, gamma=0):
     """Stores the data for the problem."""
-    print(">>> Problem formulation")
-    print(f"Alpha: {alpha} / Beta: {beta} / Gamma: {gamma}")
-    print(f'Number of vehicle: {num_veh}')
+    logger.info("Problem formulation with alpha %s, beta %s, gamma %s", alpha, beta, gamma)
+
     # planner
     planner = func(alpha, beta, gamma)
 
+    # data
     data = {}
     data["pickups_deliveries"] = pudo
     data["num_vehicles"] = num_veh
+    data["penalty"] = penalty
     data["num_stops"] = len(data["pickups_deliveries"]) * 2
     data["cost"] = np.zeros((data["num_stops"], data["num_stops"]))
     data["stops"] = list(np.array(data["pickups_deliveries"]).reshape((data["num_stops"],)))
@@ -44,24 +63,19 @@ def create_data_model(func, num_veh, pudo, alpha=1, beta=0, gamma=0):
     for idx, stop in enumerate(data["stops"]):
         data["indices_stops"][idx+1] = stop
 
-    print("Calculating cost ...")
+    logger.info("Start calculating cost")
     for i in range(data["num_stops"]):
         for j in range(data["num_stops"]):
             planner.init()
             data["cost"][i][j] = planner.astar(data["indices_stops"][i+1], data["indices_stops"][j+1])
+    logger.info("Complete cost calculation")
 
-    # set depot
+    # set depot (artificial location)
     depot = 0
-    data["depot"] = depot
     data["num_stops"] = data["num_stops"] + 1
-    data["indices_stops"][0] = depot
-    data["stops_indices"] = {v: k for k, v in data["indices_stops"].items()}
-
-    # set penalties for not visiting some stations
-    penalty = {}
-    for stop in data["stops"]:
-        penalty[data["stops_indices"][stop]] = -1000     # penalty
-    penalty[0] = -1000                                   # depot penalty
+    data["stops"].append(depot)
+    data["indices_stops"][0] = depot                                            # index -> node id
+    data["stops_indices"] = {v: k for k, v in data["indices_stops"].items()}    # node id -> index
 
     # arch's cost for a depot
     c_0 = np.zeros((1, data["cost"].shape[0]))
@@ -69,52 +83,70 @@ def create_data_model(func, num_veh, pudo, alpha=1, beta=0, gamma=0):
     c_0 = np.zeros((1, data["cost"].shape[0]))
     data["cost"] = np.concatenate((c_0.T, data["cost"]), axis=1)
 
-    return data, penalty
+    return data, planner
 
-
-def optimize(data, penalty):
+def optimize(data, working_time=None, capacity=None):
     # define and initialize the optimal model
-    m = gp.Model()      # minimize is default
+    m = gp.Model()                                                              # minimize is default
     m.Params.outputFlag = False
 
     num_v = data["num_vehicles"]
     n = data["num_stops"]
 
-    # re-definite distance matrix, t
+    # re-definite distance matrix
     dist = {}
+    dist_c = {}
     for i, row in enumerate(data["cost"]):
         for j, elem in enumerate(row):
             for v in range(data["num_vehicles"]):
                 if (i != j):
                     dist[(i, j, v)] = data["cost"][i][j]
+                    dist_c[(i, j, v)] = 0
+
+    # set pickups and dropoffs
+    pickups = [data["stops_indices"][pudo[0]] for pudo in data["pickups_deliveries"]]
+    dropoffs = [data["stops_indices"][pudo[1]] for pudo in data["pickups_deliveries"]]
 
     p = {}
-    for p_key, p_val in penalty.items():
-        for v in range(num_v):
-            p[(p_key, v)] = -1e4
+    p_c = {}
+    for i in range(n):
+        for v in range(data["num_vehicles"]):
+            p[(i, v)] = -1
+        p_c[i] = penalty
+    p_c[0] = 0
 
     # edge
-    e_vars = m.addVars(dist.keys(), obj=dist, vtype=GRB.BINARY, name="e")
-    # penalty
+    e_vars = m.addVars(dist_c.keys(), obj=dist_c, vtype=GRB.BINARY, name="e")
+    # passenger
     p_vars = m.addVars(p.keys(), obj=p, vtype=GRB.BINARY, name="p")
+    # penalty
+    pc_vars = m.addVars(p_c.keys(), obj=p_c, vtype=GRB.BINARY, name="pc")
 
     # Constraint 1: only one vehicle can visit one stop except for the depot.
-    cons1 = m.addConstrs(p_vars.sum(data["stops_indices"][i], "*") <= 1 for i in data["stops"])
+    cons1 = m.addConstrs(p_vars.sum(i, "*") <= 1 for i in range(n) if i != 0)
     # Constraint 2: visited node i must have an outgoing edge.
-    cons2 = m.addConstrs(e_vars.sum(i, "*", v) == p_vars[(i, v)]
-                         for i in list(data["indices_stops"].keys()) for v in range(num_v))
-    # Constraint 3: visited node j must an ingoing edge.
-    cons3 = m.addConstrs(e_vars.sum("*", j, v) == p_vars[(j, v)]
-                         for j in list(data["indices_stops"].keys()) for v in range(num_v))
-    # Constraint 4: considering the origin
-    cons4_1 = m.addConstr(p_vars.sum(0, "*") == num_v)
-    # Constraint 5: pickup-dropoff pairs
-    cons5_1 = m.addConstrs(
-        e_vars[(data["stops_indices"][pu], data["stops_indices"][do], v)] == p_vars[(data["stops_indices"][pu], v)]
-        for pu, do in data["pickups_deliveries"] for v in range(num_v))
-    cons5_2 = m.addConstrs(
-        p_vars[(data["stops_indices"][pu], v)] == p_vars[(data["stops_indices"][do], v)]
-        for pu, do in data["pickups_deliveries"] for v in range(num_v))
+    cons2 = m.addConstrs(e_vars.sum(i, "*", v) == p_vars[(i, v)] for i in range(n) for v in range(num_v))
+    # Constraint 3: visited node j must have an ingoing edge.
+    cons3 = m.addConstrs(e_vars.sum("*", j, v) == p_vars[(j, v)] for j in range(n) for v in range(num_v))
+    # Constraint 4: considering the origin.
+    cons4 = m.addConstr(p_vars.sum(0, "*") == num_v)
+    # Constraint 5: working time limit, capacity limit, penalty
+    if working_time:
+        cons5 = m.addConstrs(gp.quicksum(e_vars[i, j, v] * dist[(i, j, v)]
+                                         for i in range(n) for j in range(n) if i != j) <= working_time
+                             for v in range(num_v))
+    if capacity:
+        cons6 = m.addConstrs(gp.quicksum(p_vars[i, v] for i in pickups) <= capacity
+                             for v in range(num_v))
+    cons7 = m.addConstrs(1 - p_vars.sum(i, "*") == pc_vars[i] for i in range(n) if i != 0)
+    # Constraint 6: pickup-dropoff pairs
+    cons8_1 = m.addConstrs(gp.quicksum(e_vars[i, j, k]
+                                       for i in pickups
+                                       for j in range(n) if i != j and j != 0) >= gp.quicksum(e_vars[i, j, k]
+                                                                                   for i in dropoffs
+                                                                                   for j in range(n) if i != j and j != 0)
+                           for k in range(num_v))
+    cons8_2 = m.addConstrs(p_vars[pickups[i], v] <= p_vars[dropoffs[i], v] for i in range(len(pickups)) for v in range(num_v))
 
     def subtourlim(model, where):
         if where == GRB.Callback.MIPSOL:
@@ -149,22 +181,30 @@ def optimize(data, penalty):
 
                 if len(this_cycle) > 1:
                     if exclude_depot:
-                        if not (data["depot"] in this_cycle):
+                        if not (0 in this_cycle):
                             cycle[v].append(this_cycle)
         return cycle
 
     # optimize model
     m._vars = e_vars
     m._dvars = p_vars
+    m._ddvars = pc_vars
     m.Params.lazyConstraints = 1
     m.optimize(subtourlim)
 
+    # obj = m.getObjective()
+    # print(obj)
+    # print(obj.getValue())
+
     # status
-    print(">>> Result")
-    print("Solver status: ", status_dict[m.status])
+    logger.info("Solved (%s)", status_dict[m.status])
+
+    if m.status != 2:
+        sys.exit("There is no solution. Check constraints again.")
 
     e_vals = m.getAttr('x', e_vars)
 
+    # get solutions
     sol = {}
     for car in range(num_v):
         sol[car] = {}
@@ -173,37 +213,85 @@ def optimize(data, penalty):
                 sol[k][i] = j
 
     routes = []
-
+    passengers = []
+    travel_times = []
     for car in range(num_v):
         route = sol[car]
-        i = 0
+        station = 0
+        travel_time = 0
+        cap = 0
         path = []
         while True:
-            i = route[i]
-            if i == 0:
+            station_ = copy.copy(station)
+            station = route[station]
+            travel_time += data["cost"][station_][station]
+            if station == 0:
                 break
-            path.append(data["indices_stops"][i])
+            if station in pickups:
+                cap += 1
+            path.append(data["indices_stops"][station])
         routes.append(path)
-    return routes
+        passengers.append(cap)
+        travel_times.append(round(travel_time, 2))
 
-
+    return routes, passengers, travel_times
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Write the number of vehicles, three weights (alpha, beta, gamma), and passengers requests.")
-    parser.add_argument("-v", "--num_veh", type=int, required=True, help="number of vehicles")
+    parser = argparse.ArgumentParser(description="Write the number of vehicles, "
+                                                 "three weights (alpha, beta, gamma), "
+                                                 "passengers requests, "
+                                                 "working time of delivers, "
+                                                 "capacity of vehicles for delivery "
+                                                 "and penalty given for not visiting a location.")
+    parser.add_argument("-v", "--num_vehicle", type=int, required=True, help="number of vehicles")
     parser.add_argument("-w", "--weight", type=int, required=True, nargs="+", help="alpha, beta, gamma")
-    parser.add_argument("-pudo", "--pudo", type=int, required=True, nargs="+", action="append", help="passengers requests")
+    parser.add_argument("-pudo", "--pudo", type=int, required=True, nargs="+", action="append",
+                        help="passengers requests")
+    parser.add_argument("-t", "--time", type=int, required=False,
+                        help="working time of delivers in seconds")
+    parser.add_argument("-c", "--capacity", type=int, required=False,
+                        help="capacity of vehicles for delivery")
+    parser.add_argument("-p", "--penalty", type=int, required=True, help="penalty for unvisited stations")
 
     args = parser.parse_args()
 
     # make problem
-    num_veh = args.num_veh     # number of vehicle
-    alpha, beta, gamma = args.weight
-    pickup_and_dropoff = args.pudo
-    data, penalty = create_data_model(RoutingPlanner, num_veh, pickup_and_dropoff, alpha, beta, gamma)
+    num_veh = args.num_vehicle  # number of vehicles
+    penalty = args.penalty      # penalty give for not visiting a location
 
-    # solve
-    routes = optimize(data, penalty)
+    # problem setting
+    data, planner = create_data_model(func=RoutingPlanner, num_veh=num_veh, pudo=args.pudo, penalty=penalty,
+                                      alpha=args.weight[0], beta=args.weight[1], gamma=args.weight[2])
+
+    routes, passengers, travel_times = optimize(data, working_time=args.time, capacity=args.capacity)
+
+    print("[Passengers' Calls]")
+    station_user = {pudo[0]: idx for idx, pudo in enumerate(data["pickups_deliveries"])}
+    print_pudo = {"user ID": list(np.arange(len(station_user))),
+                  "pickup location": list(map(lambda x: x[0], data["pickups_deliveries"])),
+                  "dropoff location": list(map(lambda x: x[1], data["pickups_deliveries"]))}
+    print(tabulate(print_pudo, headers="keys", tablefmt="fancy_grid", missingval="N/A"))
+
+    # result
+    print("[Result]")
+    # problem description
+    print("* There is a working time limit (%s seconds) for each delivers." % (args.time))
+    print("* There is a capacity limit (%s) for vehicles." % (args.capacity))
+    print("* A penalty (%s) is given for the number of locations that cannot be visited." % (penalty))
+    print_route = {"car": [], "route": [], "users": [[] for i in range(num_veh)],
+                   "capacity": passengers, "travel time": travel_times}
+    stops = set(data["stops"])
+    stops = stops - {0}
     for car, route in enumerate(routes):
-        print(f"car: {car} | route: {route}")
+        print_route["car"].append(car)
+        print_route["route"].append(route)
+        for stop in route:
+            if stop in station_user.keys():
+                print_route["users"][car].append(station_user[stop])
+            stops = stops - {stop}
+    print(tabulate(print_route, headers="keys", tablefmt="fancy_grid", missingval="N/A"))
+    print("unvisited users: ", [station_user[stop] for stop in stops if stop in station_user.keys()])
+
+    # visualization
+    visualization(planner, print_route["route"], num_veh)
